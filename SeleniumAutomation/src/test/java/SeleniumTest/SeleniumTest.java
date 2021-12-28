b2c2e9f8747875c630b00dc99f2c28d0c8ff2554
caryregime <Regime Cary@DESKTOP-73F3N3R>
2021-12-27 19:35:01 -0700

    Added Selenium After and Before Method

--- SeleniumAutomation/src/test/java/SeleniumTest/SeleniumTest.java
+++ SeleniumAutomation/src/test/java/SeleniumTest/SeleniumTest.java
@@ -4,37 +4,39 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
+	private ChromeDriver driver;
+	private final String url = "https://www.selenium.dev/";;
+
 	@Test
 	public void canLaunchWebDriver() {
-		String driverpath = "C:\\Users\\Regime Cary\\eclipse-quintrix-training\\driver\\chromedriver.exe";
-		System.setProperty("webdriver.chrome.driver", driverpath);
-
-		String url = "https://www.selenium.dev/";
-		ChromeDriver driver = new ChromeDriver();
-
-		driver.navigate().to(url);
 		String currenturl = driver.getCurrentUrl();
-
 		Assert.assertEquals(url, currenturl);
-
-		driver.quit();
 	}
+
 	@Test
 	public void canClickButton() {
-		String driverpath = "C:\\Users\\Regime Cary\\eclipse-quintrix-training\\driver\\chromedriver.exe";
-		System.setProperty("webdriver.chrome.driver", driverpath);
-
-		String url = "https://www.selenium.dev/";
-		ChromeDriver driver = new ChromeDriver();
-
-		driver.navigate().to(url);
 		WebElement element = driver.findElement(By.cssSelector("a[href='/documentation/webdriver/']"));
-		
 		element.click();
 		driver.quit();
+	}
+
+	@BeforeMethod
+	public void setup() {
+		String driverpath = "C:\\Users\\Regime Cary\\eclipse-quintrix-training\\driver\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", driverpath);
+		this.driver = new ChromeDriver();
+		this.driver.navigate().to(url);
+	}
+	
+	@AfterMethod
+	public void cleanup() {
+		this.driver.quit();
 	}
 }
 
