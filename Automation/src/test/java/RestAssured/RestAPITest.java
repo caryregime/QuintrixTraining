4b9cdee6a271112d58465241704c1ac21d417dc1
caryregime <Regime Cary@DESKTOP-73F3N3R>
2022-01-29 20:09:50 -0700

    Added Answer for 1 and 2

--- Automation/src/test/java/RestAssured/RestAPITest.java
+++ Automation/src/test/java/RestAssured/RestAPITest.java
@@ -1,28 +1,59 @@
 package RestAssured;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
-import io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
+
 
 public class RestAPITest {
 
+	private static String URL = "https://reqres.in";
+
 	@Test public void
 	get_email_address_for_employee() {
-		String URL = "https://reqres.in/api/users/2";
-		String email = "janet.weaver@reqres.in";
-		int expectedStatusCode = 200;
-		int dataId = 2;
-		
-		given()
-			.get(URL)
-			.then()
-			.statusCode(expectedStatusCode)
-			.body("data.id[dataId].email", equalTo("janet.weaver@reqres.in"));
 
+		String expectedEmail = "janet.weaver@reqres.in";
+
+		Response response = 
+				given()
+				.when()
+				.get(URL + "/api/users/2")
+				.then()
+				.extract()
+				.response();
+
+		String actualEmail = response.path("data.email");
+
+		Assert.assertEquals(actualEmail, expectedEmail);
 	}
+
+	@Test public void
+	post_token_after_login_successful() {
+
+		String expectedToken = "QpwL5tke4Pnpja7X4";
+
+		String requestBody = "{\n" 
+				+ "   \"email\": \"eve.holt@reqres.in\",\n" 
+				+ "   \"password\": \"cityslicka\"\n" 
+				+ "}";
+
+		Response response = given()
+				.header("Content-type", "application/json")
+				.and()
+				.body(requestBody)
+				.when()
+				.post(URL + "/api/login")
+				.then()
+				.extract()
+				.response();
+
+		String actualToken = response.jsonPath().getString("token");
+
+		System.out.println(actualToken);
+
+		Assert.assertEquals(actualToken, expectedToken);
+	}
+
 
 }
