00fc125327512945c8bf009ae065fbdebde33646
caryregime <Regime Cary@DESKTOP-73F3N3R>
2022-02-10 15:52:56 -0700

    Adding TestNgProject

--- Automation/src/test/java/projectTestNg/TestNgTest.java
+++ Automation/src/test/java/projectTestNg/TestNgTest.java
@@ -1,12 +1,17 @@
 package projectTestNg;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestNgTest {
 
+	WebDriver driver = null;
+	
 	@Test
 	public void Test1() {
 		System.out.println("Test 1 done | "+Thread.currentThread().getId() );
+		System.setProperty("webdriver.chrome.driver", driverFile.toFile().getAbsolutePath());
+		driver = new ChromeDriver();
 	}
 	@Test
 	public void Test2() {
